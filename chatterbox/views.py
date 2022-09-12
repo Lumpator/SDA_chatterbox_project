81cf83075af20c1af840cdbe79507a9cd88ea093
Lumpator <vecisp@gmail.com>
2022-09-12 21:16:27 +0200

    added navbar, room view, static folder

--- chatterbox/views.py
+++ chatterbox/views.py
@@ -8,9 +8,22 @@ from chatterbox.models import Room, Message
 def hello(request, s: str):
     return HttpResponse(f"Hello, {s} world!")
 
+def home(request):
+    rooms = Room.objects.all()
+
+    context = {"rooms": rooms}
+    return render(request, "chatterbox/home.html", context)
+
 
 def search(request, s: str):
     rooms = Room.objects.filter(name__contains=s)
     messages = Message.objects.filter(body__contains=s)
 
     return render(request, "chatterbox/search.html", {"rooms": rooms, "messages": messages})
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    messages = Message.objects.filter(room=pk)
+
+    context = {"room": room, "messages": messages}
+    return render(request, "chatterbox/room.html", context)
